5fb19a8f6ee2a462aed686126e244373916ffb45
Mahmood2739 <202202839@student.polytechnic.bh>
2026-06-02 02:33:11 +0300

    Add JWT settings and authentication service

---
+++ TrainingCertification.API/Helpers/JwtSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace TrainingCertification.API.Helpers;
+
+public class JwtSettings
+{
+    public string Issuer { get; set; } = "TrainingCertificationPlatform";
+    public string Audience { get; set; } = "TrainingCertificationPlatformUsers";
+    public string Key { get; set; } = "TrainingCertificationPlatform-Demo-Key-Change-In-Production-2026";
+    public string Secret { get => Key; set => Key = value; }
+    public int ExpiryMinutes { get; set; } = 120;
+}
